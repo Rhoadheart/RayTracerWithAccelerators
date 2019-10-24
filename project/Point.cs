e21af0ac4ec8712d23d145e86edf1379c5fae294
Rhoadheart <rhoadead@plu.edu>
2019-10-24 13:26:36 -0700

    Implemented Methods in the Point Class and Ray Class

--- project/Point.cs
+++ project/Point.cs
@@ -8,15 +8,16 @@ namespace project
 {
     class Point
     {
-        int x;
-        int y;
-        int z;
+        public float x;
+        public float y;
+        public float z;
 
-        public Point(int x, int y, int z)
+        public Point(float x, float y, float z)
         {
             this.x = x;
             this.y = y;
             this.z = z;
         }
+        
     }
 }
--- project/Ray.cs
+++ project/Ray.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace project.RayTracing
 {
     class Ray
     {
+        Point origin;
+        Vector3 direction;
+
+        public Ray(Point origin, Vector3 direction)
+        {
+            this.origin = origin;
+            this.direction = Vector3.Normalize(direction);
+        }
+
+        /**
+         * <summary>
+         * Returns a point along the ray at a given distance.
+         * </summary>
+         * 
+         */ 
+        public Point at(double dist)
+        {
+            Vector3 output = Vector3.Add(Vector3.Multiply(direction, (float)dist), new Vector3(origin.x, origin.y, origin.z));
+            return new Point(output.X, output.Y, output.Z);
+        }
+
     }
 }
