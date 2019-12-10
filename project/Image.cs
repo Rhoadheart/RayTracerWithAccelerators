a43e33f742d2aee5b4f87854ad3c5943f50e2421
Rhoadheart <rhoadead@plu.edu>
2019-12-09 21:17:58 -0800

    Code cleanup

--- project/Image.cs
+++ project/Image.cs
@@ -67,11 +67,7 @@ namespace project.RayTracing
                 {
                     Color newColor;
                     Ray r = c.getRay(new Vector2(i,j));
-
-                    if (i == 825 && j == 678)
-                    {
-                        RV.updateBitmap(b);
-                    }
+                    
 
                     Triangle intersect = null;
                     if(accelStruct == "Grid")
