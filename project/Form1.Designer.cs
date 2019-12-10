67e1423b7857b4373592890cbae003d153199cb4
Rhoadheart <rhoadead@plu.edu>
2019-12-10 09:42:05 -0800

    Removed old test button for clean demo

--- project/Form1.Designer.cs
+++ project/Form1.Designer.cs
@@ -33,7 +33,6 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
             this.GoodGenLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -78,16 +77,6 @@
             this.label2.Size = new System.Drawing.Size(0, 17);
             this.label2.TabIndex = 5;
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(298, 12);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Test";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // GoodGenLabel
             // 
             this.GoodGenLabel.AutoSize = true;
@@ -103,7 +92,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(385, 177);
             this.Controls.Add(this.GoodGenLabel);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
@@ -122,7 +110,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label GoodGenLabel;
     }
 }
